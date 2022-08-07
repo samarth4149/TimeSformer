3ca475c1c1dd94510f65b2a8d9d6f5c2ed3f5d22
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-08-06 23:20:32 -0400

    added code to write moments filelist

---
+++ test_moments.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from pathlib import Path
+import csv
+from tqdm import tqdm
+
+
+if __name__ == '__main__':
+    
+    data_file_base = Path('/gpfs/u/home/DPLD/DPLDsmms/scratch-shared/datasets/Moments')
+    data_file_paths = [data_file_base / 'moments150_kinetics_overlap_train.txt', data_file_base / 'moments150_kinetics_overlap_val.txt']
+    vid_base = Path('/gpfs/u/home/DPLD/DPLDsmms/scratch-shared/datasets/Moments/videos')
+
+    out_dir = Path('data_files/minimoments')
+    os.makedirs(out_dir, exist_ok=True)
+    
+    for df in data_file_paths:
+        if 'train' in df.name:
+            outfile = out_dir / 'train.csv'
+            split = 'train'
+        else:
+            outfile = out_dir / 'val.csv'
+            split = 'val'
+        with open(df, 'r') as f, open(outfile, 'w') as fout:
+            reader = csv.reader(f, delimiter=';')
+            for row in reader:
+                fname = row[0]
+                label = row[3]
+                curr_path = vid_base / split / ('/'.join(fname.split('/')[1:]) + '.mp4')
+                if not os.path.exists(curr_path):
+                    raise Exception(f'{curr_path} does not exist')
+                
+                print(f'{curr_path};{label}', file=fout)
+    
