c36424380e83803a4fbaaf0897ee90681cf6660e
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-08-08 16:05:30 -0400

    small launch change

--- slurm_scripts/downstream/from_kinetics_scratch.sh
+++ slurm_scripts/downstream/from_kinetics_scratch.sh
@@ -1,6 +1,6 @@
 #### Kinetics training
-JOB_NAME=ucf101_finetune
-python tools/submit.py --cfg configs/Downstream/ucf101_finetune.yaml --job_dir  expts/downstream/from_kinetics_scratch/${JOB_NAME}/  --num_shards 1 --num_gpus 4 --name ${JOB_NAME} TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/MiniKinetics_TimeSformer_divST_8x32_224_scratch/checkpoints/checkpoint_epoch_00030.pyth
+# JOB_NAME=ucf101_finetune
+# python tools/submit.py --cfg configs/Downstream/ucf101_finetune.yaml --job_dir  expts/downstream/from_kinetics_scratch/${JOB_NAME}/  --num_shards 1 --num_gpus 4 --name ${JOB_NAME} TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/MiniKinetics_TimeSformer_divST_8x32_224_scratch/checkpoints/checkpoint_epoch_00030.pyth
 
 JOB_NAME=ucf101_lin_probe
 python tools/submit.py --cfg configs/Downstream/ucf101_lin_probe.yaml --job_dir  expts/downstream/from_kinetics_scratch/${JOB_NAME}/  --num_shards 1 --num_gpus 4 --name ${JOB_NAME} TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/MiniKinetics_TimeSformer_divST_8x32_224_scratch/checkpoints/checkpoint_epoch_00030.pyth
