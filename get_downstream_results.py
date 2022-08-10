c8feeab22b10dc0df8129909f60a8fb1e98b33b8
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-08-10 15:31:44 -0400

    added get_downstream_results script

---
+++ get_downstream_results.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.append(os.path.abspath('.'))
+import pandas as pd
+import json
+
+if __name__ == '__main__':
+    downstream_datasets = ['uav', 'ikea_furniture', 'diving48', 'ucf101', 'hmdb51', 'mini_ssv2']
+    pt_methods = ['kinetics_pt', 'kinetics_scratch']
+    downstream_modes = ['lin_probe', 'finetune']
+    df = pd.DataFrame(columns=downstream_datasets, index=pd.MultiIndex.from_product([pt_methods, downstream_modes], names=['pt_method', 'mode']))
+    
+    for p in pt_methods:
+        for m in downstream_modes:
+            for d in downstream_datasets:
+                curr_path = f'expts/downstream/from_{p}/{d}_{m}/stdout.log'
+                with open(curr_path, 'r') as f:
+                    lines = f.read().splitlines()
+                    for l in lines[::-1]:
+                        if '{' in l:
+                            log_dict = json.loads('{' + l.split('{')[1])
+                            if log_dict['_type']!='val_epoch':
+                                print('Something wrong with log at {}'.format(curr_path))
+                            else:
+                                df.loc[(p, m), d] = 100. - float(log_dict['top1_err'])
+                            break                            
+
+    df.to_csv('downstream_results.csv')
