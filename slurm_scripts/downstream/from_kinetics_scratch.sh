9da8600f3b8265a419837cefe900206204c293f2
Samarth Mishra <samarth4149@users.noreply.github.com>
2022-08-15 15:41:33 -0400

    ready to complete diving 48 linear probe

--- slurm_scripts/downstream/from_kinetics_scratch.sh
+++ slurm_scripts/downstream/from_kinetics_scratch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #### Kinetics training
 # for dset in ucf101 uav ikea_furniture diving48 hmdb51 mini_ssv2;
-for dset in mini_ssv2;
+for dset in diving48;
 do
-    JOB_NAME=${dset}_finetune
-    python tools/submit.py --cfg configs/Downstream/${dset}_finetune.yaml --job_dir  expts/downstream/from_kinetics_scratch/${JOB_NAME}/  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} TRAIN.FINETUNE False
+    # JOB_NAME=${dset}_finetune
+    # python tools/submit.py --cfg configs/Downstream/${dset}_finetune.yaml --job_dir  expts/downstream/from_kinetics_scratch/${JOB_NAME}/  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} TRAIN.FINETUNE False
     # TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/MiniKinetics_TimeSformer_divST_8x32_224_scratch/checkpoints/checkpoint_epoch_00030.pyth
 
     JOB_NAME=${dset}_lin_probe
