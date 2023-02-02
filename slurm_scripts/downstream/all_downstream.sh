7c0b2a509c883d80ca82e213c5a6c69b2d33c7e6
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-02 12:02:34 +0530

    naming fix

--- slurm_scripts/downstream/all_downstream.sh
+++ slurm_scripts/downstream/all_downstream.sh
@@ -2,10 +2,10 @@ for backbone_dir in MiniSynthetic_vit_s_in1k_pt MiniSynthetic_vit_s_pass_dino_pt
 do
     for dset in ucf101 uav ikea_furniture diving48 hmdb51 mini_ssv2;
     do
-        JOB_NAME=${backbone_dir}_{dset}_finetune
+        JOB_NAME=${backbone_dir}_${dset}_finetune
         python tools/submit.py --cfg configs/Downstream/${dset}_finetune.yaml --job_dir  expts/downstream/from_${backbone_dir}/${JOB_NAME}/  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/${backbone_dir}/checkpoints/checkpoint_epoch_00075.pyth
 
-        JOB_NAME=${backbone_dir}_{dset}_lin_probe
+        JOB_NAME=${backbone_dir}_${dset}_lin_probe
         python tools/submit.py --cfg configs/Downstream/${dset}_lin_probe.yaml --job_dir  expts/downstream/from_${backbone_dir}/${JOB_NAME}/  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/${backbone_dir}/checkpoints/checkpoint_epoch_00075.pyth;
     done
 done
