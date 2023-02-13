9a3eec20683092518a9a3ce0c1b0606e22602135
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-13 20:41:38 +0530

    restarting a run

--- slurm_scripts/downstream/downstream_hp.sh
+++ slurm_scripts/downstream/downstream_hp.sh
@@ -4,9 +4,9 @@ do
     # for dset in ucf101 uav ikea_furniture diving48 hmdb51 mini_ssv2;
     for dset in hmdb51;
     do
-        for base_lr in 1.e-3 3.e-6 1.e-4 3.e-5 1.e-5;
+        for base_lr in 1.e-4;
         do
-            for head_factor in 1. 10.;
+            for head_factor in 10.;
             do
                 base_lr_str=$(printf "%.0e\n" $base_lr) 
                 head_factor_str=$(printf "%.0e\n" $head_factor) 
