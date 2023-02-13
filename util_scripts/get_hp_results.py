852d7079a17c422f4a39cbd4b9f45b1470041a64
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-13 10:07:26 +0530

    new hp results

--- util_scripts/get_hp_results.py
+++ util_scripts/get_hp_results.py
@@ -9,30 +9,30 @@ if __name__ == '__main__':
     # downstream_datasets = ['ucf101', 'hmdb51', 'mini_ssv2', 'diving48', 'ikea_furniture', 'uav',]
     downstream_datasets = ['hmdb51']
     pt_methods = ['MiniSynthetic_vit_s_pass_dino_pt', 'MiniSynthetic_vit_s_pass_dino_pt_tatt_only_ft']
-    downstream_modes = ['finetune']
+    downstream_modes = ['linprobe']
     base_lrs = [1.e-3, 3.e-4, 1.e-4]
-    head_factors = [1., 10.]
-    df = pd.DataFrame(columns=downstream_datasets, index=pd.MultiIndex.from_product([pt_methods, downstream_modes, base_lrs, head_factors], names=['pt_method', 'mode', 'base_lr', 'head_factor']))
+    # head_factors = [1., 10.]
+    df = pd.DataFrame(columns=downstream_datasets, index=pd.MultiIndex.from_product([pt_methods, downstream_modes, base_lrs], names=['pt_method', 'mode', 'base_lr', 'head_factor']))
     
     for p in pt_methods:
         for m in downstream_modes:
             for d in downstream_datasets:
-                for b in base_lrs:
-                    for h in head_factors:
-                        curr_path = f'expts/downstream/hp_tune/from_{p}/{d}_{m}/base_lr_{b:.0e}_head_factor_{h:.0e}/stdout.log'
-                        with open(curr_path, 'r') as f:
-                            lines = f.read().splitlines()
-                            for l in lines[::-1]:
-                                if '{' in l:
-                                    try:
-                                        log_dict = json.loads('{' + l.split('{')[1])
-                                    except JSONDecodeError:
-                                        print('Something wrong with log at {}'.format(curr_path))
-                                        break
-                                    if log_dict['_type']!='val_epoch' and log_dict['epoch']!='20/20':
-                                        print('Something wrong with log at {}'.format(curr_path))
-                                    else:
-                                        df.loc[(p, m, b, h), d] = 100. - float(log_dict['top1_err'])
-                                    break                            
+                for b in [0.01, 0.003, 0.001, 0.0003, 0.0001]:
+                    # for h in head_factors:
+                    curr_path = f'expts/downstream/hp_tune/from_{p}/{d}_{m}/base_lr_{b:.0e}/stdout.log'
+                    with open(curr_path, 'r') as f:
+                        lines = f.read().splitlines()
+                        for l in lines[::-1]:
+                            if '{' in l:
+                                try:
+                                    log_dict = json.loads('{' + l.split('{')[1])
+                                except JSONDecodeError:
+                                    print('Something wrong with log at {}'.format(curr_path))
+                                    break
+                                if log_dict['_type']!='val_epoch' and log_dict['epoch']!='20/20':
+                                    print('Something wrong with log at {}'.format(curr_path))
+                                else:
+                                    df.loc[(p, m, b), d] = 100. - float(log_dict['top1_err'])
+                                break                            
 
                 df.to_csv(f'expts/downstream/hp_tune/from_{p}/{d}_{m}/hp_results.csv')
