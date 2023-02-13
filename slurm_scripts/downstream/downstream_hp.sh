6b9ea1df4fb268b4ed1c0c426d9212599e3e5bdd
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-13 17:28:54 +0530

    ready to run lp ft hyperparams tuning

--- slurm_scripts/downstream/downstream_hp.sh
+++ slurm_scripts/downstream/downstream_hp.sh
@@ -4,17 +4,20 @@ do
     # for dset in ucf101 uav ikea_furniture diving48 hmdb51 mini_ssv2;
     for dset in hmdb51;
     do
-        for base_lr in 3.e-1 1.e-1 3.e-2;
+        for base_lr in 1.e-3 3.e-6 1.e-4 3.e-5 1.e-5;
         # do
-        #     for head_factor in 10.;
+            for head_factor in 1. 10.;
         do
             base_lr_str=$(printf "%.0e\n" $base_lr) 
-            # head_factor_str=$(printf "%.0e\n" $head_factor) 
+            head_factor_str=$(printf "%.0e\n" $head_factor) 
             # JOB_NAME=${backbone_dir}_${dset}_finetune_base_lr_${base_lr_str}_head_factor_${head_factor_str}
             # python tools/submit.py --cfg configs/Downstream/${dset}_finetune.yaml --job_dir expts/downstream/hp_tune/from_${backbone_dir}/${dset}_finetune/base_lr_${base_lr_str}_head_factor_${head_factor_str}  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} MODEL.MODEL_NAME vit_small_patch16_224 TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/${backbone_dir}/checkpoints/checkpoint_epoch_00075.pyth SOLVER.BASE_LR ${base_lr} SOLVER.HEAD_LR_FACTOR ${head_factor}
 
-            JOB_NAME=${backbone_dir}_${dset}_linprobe_base_lr_${base_lr_str}
-            python tools/submit.py --cfg configs/Downstream/${dset}_lin_probe.yaml --job_dir expts/downstream/hp_tune/from_${backbone_dir}/${dset}_linprobe/base_lr_${base_lr_str}  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} MODEL.MODEL_NAME vit_small_patch16_224 TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/${backbone_dir}/checkpoints/checkpoint_epoch_00075.pyth SOLVER.BASE_LR ${base_lr}
+            JOB_NAME=${backbone_dir}_${dset}_finetune_base_lr_${base_lr_str}_head_factor_${head_factor_str}
+            python tools/submit.py --cfg configs/Downstream/${dset}_finetune.yaml --job_dir expts/downstream/hp_tune/from_${backbone_dir}/${dset}_lpft/base_lr_${base_lr_str}_head_factor_${head_factor_str}  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} MODEL.MODEL_NAME vit_small_patch16_224 TRAIN.CHECKPOINT_FILE_PATH expts/downstream/hp_tune/from_${backbone_dir}/${dset}_linprobe/base_lr_1e-2/checkpoints/checkpoint_epoch_00020.pyth SOLVER.BASE_LR ${base_lr} SOLVER.HEAD_LR_FACTOR ${head_factor}
+
+            # JOB_NAME=${backbone_dir}_${dset}_linprobe_base_lr_${base_lr_str}
+            # python tools/submit.py --cfg configs/Downstream/${dset}_lin_probe.yaml --job_dir expts/downstream/hp_tune/from_${backbone_dir}/${dset}_linprobe/base_lr_${base_lr_str}  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} MODEL.MODEL_NAME vit_small_patch16_224 TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/${backbone_dir}/checkpoints/checkpoint_epoch_00075.pyth SOLVER.BASE_LR ${base_lr}
 
             # JOB_NAME=${backbone_dir}_${dset}_lin_probe
             # python tools/submit.py --cfg configs/Downstream/${dset}_lin_probe.yaml --job_dir  expts/downstream/from_${backbone_dir}/${JOB_NAME}/  --num_shards 2 --num_gpus 4 --name ${JOB_NAME} MODEL.MODEL_NAME vit_small_patch16_224 TRAIN.CHECKPOINT_FILE_PATH /gpfs/u/home/DPLD/DPLDsmms/scratch/projects/TimeSformer/expts/${backbone_dir}/checkpoints/checkpoint_epoch_00075.pyth;
