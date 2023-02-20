5e0d746f90cebcd21a8706840071be06218b11a8
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-20 18:56:16 -0500

    running more step 3

--- slurm_scripts/run_step3.sh
+++ slurm_scripts/run_step3.sh
@@ -1,45 +1,63 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 # A script with a list of commands for submitting SLURM jobs
 
-#### Synthetic training
-# Imagenet 1K training
-# JOB_NAME=MiniSynthetic_vit_s_in1k_pt
-# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pt.yaml --job_dir expts/${JOB_NAME}/ --num_shards 4 --num_gpus 4 --name ${JOB_NAME}
+# Synthetic training
+# # Imagenet 1K MAE pretrained
+# JOB_NAME=MiniSynthetic_step3_in1k_mae_pt_ft
+# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_in1k_400.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 1
 
-# Scratch training
-# JOB_NAME=MiniSynthetic_vit_s_scratch
-# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_scratch.yaml --job_dir expts/${JOB_NAME}/ --num_shards 4 --num_gpus 4 --name ${JOB_NAME}
+# JOB_NAME=MiniSynthetic_step3_in1k_mae_pt_tatt_only_ft
+# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_in1k_400.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 1
 
-# JOB_NAME=MiniSynthetic_vit_s_pass_dino_pt_tatt_only_ft
-# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/${JOB_NAME}/ --num_shards 4 --num_gpus 4 --name ${JOB_NAME}
+# JOB_NAME=MiniSynthetic_step3_in1k_mae_pt_stadapter
+# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_in1k_400.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 1
 
-JOB_NAME=MiniSynthetic_step3_in1k_mae_pt_ft
-python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_in1k_400.pt TRAIN.BATCH_SIZE 16
+# # PASS MAE pretrained
+# JOB_NAME=MiniSynthetic_step3_pass_mae_pt_ft
+# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_pass_400.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 1
 
-JOB_NAME=MiniSynthetic_step3_in1k_mae_pt_tatt_only_ft
-python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_in1k_400.pt TRAIN.BATCH_SIZE 16
+# JOB_NAME=MiniSynthetic_step3_pass_mae_pt_tatt_only_ft
+# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_pass_400.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 1
 
-JOB_NAME=MiniSynthetic_step3_in1k_mae_pt_stadapter
-python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_in1k_400.pt TRAIN.BATCH_SIZE 16
+# JOB_NAME=MiniSynthetic_step3_pass_mae_pt_stadapter
+# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/mae_pass_400.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 1
 
-# JOB_NAME=MiniSynthetic_vit_s_pass_dino_pt
-# python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/${JOB_NAME}/ --num_shards 4 --num_gpus 4 --name ${JOB_NAME}
+# PASS - K150inp
+JOB_NAME=MiniSynthetic_step3_pass_mae_k150inp_mae_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/omnimae_vitb_b128_ep200_NB256_ckpt:vitb_b512_ep400_mae_pass-load_ckpt:no_heads.yaml_dset_mae_inpk150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
 
-#JOB_NAME=TimeSformer_jointST_8x32_224
-#python tools/submit.py --cfg configs/Kinetics/TimeSformer_jointST_8x32_224.yaml --job_dir  /your/job/dir/${JOB_NAME}/   --num_shards 4 --partition learnfair --comment "" --name ${JOB_NAME} --use_volta32
+JOB_NAME=MiniSynthetic_step3_pass_mae_k150inp_mae_tatt_only_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/omnimae_vitb_b128_ep200_NB256_ckpt:vitb_b512_ep400_mae_pass-load_ckpt:no_heads.yaml_dset_mae_inpk150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
 
-#JOB_NAME=TimeSformer_spaceOnly_8x32_224
-#python tools/submit.py --cfg configs/Kinetics/TimeSformer_spaceOnly_8x32_224.yaml --job_dir  /your/job/dir/${JOB_NAME}/   --num_shards 4 --partition learnfair --comment "" --name ${JOB_NAME} --use_volta32
+JOB_NAME=MiniSynthetic_step3_pass_mae_k150inp_mae_stadapter
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/omnimae_vitb_b128_ep200_NB256_ckpt:vitb_b512_ep400_mae_pass-load_ckpt:no_heads.yaml_dset_mae_inpk150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
 
-#### Kinetics inference
-#JOB_NAME=TimeSformer_divST_8x32_224_TEST_3clips
-#python tools/submit.py --cfg configs/Kinetics/TimeSformer_divST_8x32_224_TEST.yaml --job_dir /your/job/dir/${JOB_NAME}/  --num_shards 4 --partition dev --comment "" --name ${JOB_NAME} --use_volta32
+# - K150inp
+JOB_NAME=MiniSynthetic_step3_k150inp_mae_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/omnimae_vitb_b128_ep200_NB256__dset_mae_inpk150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
 
+JOB_NAME=MiniSynthetic_step3_k150inp_mae_tatt_only_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/omnimae_vitb_b128_ep200_NB256__dset_mae_inpk150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
 
-##### SSv2 training
-#JOB_NAME=TimeSformer_divST_8_224
-#python tools/submit.py --cfg configs/SSv2/TimeSformer_divST_8_224.yaml --job_dir  /your/job/dir/${JOB_NAME}/   --num_shards 4 --partition learnfair --comment "" --name ${JOB_NAME} --use_volta32
+JOB_NAME=MiniSynthetic_step3_k150inp_mae_stadapter
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL /gpfs/u/home/DPLD/DPLDhwrg/scratch/TimeSformer/ccc_models/omnimae_vitb_b128_ep200_NB256__dset_mae_inpk150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
 
-##### Sth-Sth_v2 inference
-#JOB_NAME=TimeSformer_divST_8_224_TEST_3clips
-#python tools/submit.py --cfg configs/SSv2/TimeSformer_divST_8_224_TEST.yaml --job_dir  /your/job/dir/${JOB_NAME}/   --num_shards 4 --partition learnfair --comment "" --name ${JOB_NAME} --use_volta32
+# Imagenet-1K - K150 MAE
+JOB_NAME=MiniSynthetic_step3_in1k_mae_k150_mae_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL omnimae_vitb_b128_ep200_NB256_ckpt:vitb_b512_ep400_mae_ini1k-load_ckpt:no_heads.yaml_dset_mae_k150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
+
+JOB_NAME=MiniSynthetic_step3_in1k_mae_k150_mae_tatt_only_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL omnimae_vitb_b128_ep200_NB256_ckpt:vitb_b512_ep400_mae_ini1k-load_ckpt:no_heads.yaml_dset_mae_k150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
+
+JOB_NAME=MiniSynthetic_step3_in1k_mae_k150_mae_stadapter
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL omnimae_vitb_b128_ep200_NB256_ckpt:vitb_b512_ep400_mae_ini1k-load_ckpt:no_heads.yaml_dset_mae_k150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
+
+# - K150 MAE
+JOB_NAME=MiniSynthetic_step3_k150_mae_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt.yaml --job_dir expts/step3/${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL omnimae_vitb_b128_ep200_NB256__dset_mae_k150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
+
+JOB_NAME=MiniSynthetic_step3_k150_mae_tatt_only_ft
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_tatt_only_ft.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL omnimae_vitb_b128_ep200_NB256__dset_mae_k150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
+
+JOB_NAME=MiniSynthetic_step3_k150_mae_stadapter
+python tools/submit.py --cfg configs/MiniSynthetic/TimeSformer_divST_8x32_224_pass_pt_st_adapter.yaml --job_dir expts/step3//${JOB_NAME}/ --num_shards 1 --num_gpus 4 --name ${JOB_NAME} DATA.NUM_FRAMES 16 MODEL.MODEL_NAME vit_base_patch16_224 TIMESFORMER.PRETRAINED_MODEL omnimae_vitb_b128_ep200_NB256__dset_mae_k150_config_200.pt TRAIN.BATCH_SIZE 16 TRAIN.DEL_INTERMEDIATE_CHECKPOINTS False TRAIN.CHECKPOINT_PERIOD 5
