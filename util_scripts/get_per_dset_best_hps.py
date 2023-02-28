dcd921d4d7cf0c7d1a89af6d1a500100ef42dc48
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-28 08:50:45 -0500

    ready to get per dset best hps

--- util_scripts/get_per_dset_best_hps.py
+++ util_scripts/get_per_dset_best_hps.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     downstream_modes = ['finetune']
     # downstream_modes = ['lpft']
     # base_lrs = [0.3, 0.1, 0.03, 0.01, 0.003, 0.001, 0.0003, 0.0001, 3.e-5, 1.e-5, 3.e-6, 1.e-6]
-    base_lrs = [0.1, 0.01]
-    # head_factors = [1., 10.]
+    base_lrs = [0.01, 0.001, 0.0001]
+    head_factors = [10.]
     
     for m in downstream_modes:
         df = pd.DataFrame(columns=downstream_datasets, index=pd.MultiIndex.from_product([pt_methods, base_lrs], names=['pt_method', 'base_lr']))
@@ -31,7 +31,8 @@ if __name__ == '__main__':
                 # df = pd.DataFrame(columns=downstream_datasets, index=base_lrs)
                 # df.index.name = 'base_lr'
                 for b in base_lrs:
-                    curr_path = f'expts/downstream/hp_tune/from_{p}/{d}_{m}/base_lr_{b:.0e}/stdout.log'
+                    
+                    curr_path = f'expts/downstream/hp_tune/from_{p}/{d}_{m}/base_lr_{b:.0e}_head_factor_{10.:.0e}/stdout.log'
                     with open(curr_path, 'r') as f:
                         lines = f.read().splitlines()
                         for l in lines[::-1]:
