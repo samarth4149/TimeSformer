014390205ce6da0bc2842c9f27e03871b019ba49
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-28 09:04:40 -0500

    ready to get downstream results

--- util_scripts/get_downstream_results.py
+++ util_scripts/get_downstream_results.py
@@ -7,7 +7,7 @@ from json import JSONDecodeError
 
 if __name__ == '__main__':
     downstream_datasets = ['ucf101', 'hmdb51', 'mini_ssv2', 'diving48', 'ikea_furniture', 'uav',]
-    pt_methods = ['MiniSynthetic_vit_s_in1k_pt', 'MiniSynthetic_vit_s_pass_dino_pt', 'MiniSynthetic_vit_s_pass_dino_pt_tatt_only_ft', 'MiniSynthetic_vit_s_scratch']
+    pt_methods = ['MiniSynthetic_step3_k150inp_mae_stadapter', 'MiniKinetics_step3_k150_mae_ft']
     downstream_modes = ['lin_probe', 'finetune']
     df = pd.DataFrame(columns=downstream_datasets, index=pd.MultiIndex.from_product([pt_methods, downstream_modes], names=['pt_method', 'mode']))
     
@@ -30,4 +30,4 @@ if __name__ == '__main__':
                                 df.loc[(p, m), d] = 100. - float(log_dict['top1_err'])
                             break                            
 
-    df.to_csv('downstream_results.csv')
+    df.to_csv('misc_results/downstream_results.csv')
