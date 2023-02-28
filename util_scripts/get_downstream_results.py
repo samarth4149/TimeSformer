6aaeb7476281817042e022e37c1570b5be1a8f74
Samarth Mishra <samarth4149@users.noreply.github.com>
2023-02-28 09:17:58 -0500

    small fix

--- util_scripts/get_downstream_results.py
+++ util_scripts/get_downstream_results.py
@@ -9,14 +9,15 @@ if __name__ == '__main__':
     downstream_datasets = ['ucf101', 'hmdb51', 'mini_ssv2', 'diving48', 'ikea_furniture', 'uav',]
     # downstream_datasets = ['ucf101', 'hmdb51', 'diving48']
     # pt_methods = ['MiniSynthetic_step3_k150inp_mae_stadapter', 'MiniKinetics_step3_k150_mae_ft']
-    pt_methods = ['MiniKinetics_step3_k150_mae_ft_step25', 'MiniKinetics_step3_k150_mae_ft_step50']
+    pt_methods = ['MiniKinetics_step3_k150_mae_ft_epoch25', 'MiniKinetics_step3_k150_mae_ft_epoch50']
     downstream_modes = ['lin_probe']
     df = pd.DataFrame(columns=downstream_datasets, index=pd.MultiIndex.from_product([pt_methods, downstream_modes], names=['pt_method', 'mode']))
     
     for p in pt_methods:
         for m in downstream_modes:
             for d in downstream_datasets:
-                curr_path = f'expts/downstream/from_{p}/{p}_{d}_{m}/stdout.log'
+                # curr_path = f'expts/downstream/from_{p}/{p}_{d}_{m}/stdout.log'
+                curr_path = f'expts/downstream/from_MiniKinetics_step3_k150_mae_ft/{p}_{d}_{m}/stdout.log'
                 with open(curr_path, 'r') as f:
                     lines = f.read().splitlines()
                     for l in lines[::-1]:
